7a7863432a6c20a1d624ab0f8e60d7d7a5f56d6e
ninjaprogrammer2999 <149578035+ninjaprogrammer2999@users.noreply.github.com>
2024-05-16 22:55:58 +0530

    py9

---
+++ py9.py
@@ -0,0 +1,53 @@
+#WHILE LOOPS IN PYTHON!
+#while = a statement which runs it's block of code, as long as it'S condition remains true!
+
+#while 1==1:
+ #   print("help! i'm stuck in a loop!") #this is an infinite loop there is no escape from this!
+
+#name = ""
+#while len(name)==0:
+ #   name = input("enter your name: ")
+
+#print("hello "+name)
+
+#while 1==1:
+#    print("pls don't spam sh!t")
+
+#name = ""
+
+#while len(name) == 0:
+#    name = input("enter your name:")
+
+#print("hello "+name)
+
+# another way of writing this
+
+#name = None
+
+#while not name:
+#    name = input("enter your name: ")
+
+#print("Hello "+name)
+
+# for loop = a statement that execute it's block of code a limited amount of times
+#             while = ulimited! on cnditions
+#             for = limited!
+
+
+#for i in range(10):
+ #   print(i+1)
+
+#for i in range(100):
+#    print(i+1)
+
+#for i in range(25,105,10):
+    #in range function you can add 3rd argument
+    #step: you can  add how much you wanna count up or count down (here it's 10 up)
+#    print(i)
+#***#benefit of for loop is we can iterate anything that can iterable!***
+
+#for i in "Nissan":  #str this time!
+##    print(i)
+
+#for i in "mossow":
+#    print(i)
